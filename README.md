e4d2b64a229e968aeb95cf0d5332c8f5b5ca6395
innobrightcafe <innobrightcafe@gmail.com>
2023-12-17 15:04:56 -0500

    markdown

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Frontend for fun
+# ALX Frontend for fun
+
+__I had fun writing these stuffs. thanks ALX.__
+
+_This is a script to convert a Markdown file to HTML._
+
+Usage:
+    ./markdown2html.py [input_file] [output_file]
+
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
+
+Example:
+    ./markdown2html.py README.md README.html.
--- markdown2html.py
+++ markdown2html.py
@@ -1,32 +1,63 @@
-#!usr/bimport sys
-import markdown
-import os
+#!/usr/bin/python3
+"""
+This is a script to convert a Markdown file to HTML.
 
-def convert_markdown_to_html(input_file, output_file):
-    with open(input_file, 'r') as md_file:
-        markdown_text = md_file.read()
+Usage:
+    ./markdown2html.py [input_file] [output_file]
 
-    html_text = markdown.markdown(markdown_text)
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
 
-    with open(output_file, 'w') as html_file:
-        html_file.write(html_text)
+Example:
+    ./markdown2html.py README.md README.html
+"""
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py <input_markdown_file> <output_html_file>\n")
+import argparse
+import pathlib
+import re
+import sys
+
+
+def convert_md_to_html(input_file, output_file):
+    '''
+    To converts markdown file to HTML file
+    '''
+    # Read the contents of input file
+    with open(input_file, encoding='utf-8') as f:
+        md_content = f.readlines()
+
+    html_content = []
+    for line in md_content:
+        # Check if line is a heading
+        match = re.match(r'(#){1,6} (.*)', line)
+        if match:
+            # Get the level of heading
+            h_level = len(match.group(1))
+            # Get the content of heading
+            h_content = match.group(2)
+            # Append the HTML equivalent
+            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+        else:
+            html_content.append(line)
+
+    # Write content to the output file
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.writelines(html_content)
+
+
+if __name__ == '__main__':
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
+    parser.add_argument('input_file', help='path to input markdown file')
+    parser.add_argument('output_file', help='path to output HTML file')
+    args = parser.parse_args()
+
+    # Check if the input file exists
+    input_path = pathlib.Path(args.input_file)
+    if not input_path.is_file():
+        print(f'Missing {input_path}', file=sys.stderr)
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    if not os.path.exists(input_file):
-        sys.stderr.write(f"Missing {input_file}\n")
-        sys.exit(1)
-
-    try:
-        convert_markdown_to_html(input_file, output_file)
-        sys.exit(0)
-    except Exception as e:
-        sys.stderr.write(f"An error occurred: {e}\n")
-        sys.exit(1)
-        
+    # Convert the markdown file to HTML
+    convert_md_to_html(args.input_file, args.output_file)
